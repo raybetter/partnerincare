11723886bcde306430aebf834eaf497e10a49e12
Justin Ray <redacted@example.com>
2024-07-24 22:58:23 -0400

    wip setting up landing page

---
+++ notes.md
@@ -0,0 +1,16 @@
+
+
+Quick start from: https://www.gatsbyjs.com/docs/quick-start/
+
+Had to install git for npm gatsby init to complete.
+
+too complicated
+
+Trying: https://www.youtube.com/watch?v=00gyCtIQp8E
+
+with repo at: https://github.com/johnkomarnicki/modern-landing-page-with-tailwinds/tree/master
+
+https://tailwindcss.com/docs/installation
+
+npm install -D tailwindcss
+npx tailwindcss init
---
+++ site/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "ak-green": "#2f8b19",
+        "ak-orange": "#ef834d",
+        "ak-vlight-orange": "#ffe0c4",
+        "ak-light-orange": "#fdd7c3",
+        "ak-dark-orange": "#b45f06",
+      }
+    },
+    fontFamily: {
+      Convergence: ["Convergence"],
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px"
+      }
+    }
+  },
+  plugins: [],
+}
+
+
